f01e3f16aee1c5dbf67ab49738a9c2ae549a36db
Kernautist <86243227+Kernautist@users.noreply.github.com>
2021-06-28 12:15:19 +0200

    Update set_display.py

--- set_display.py
+++ set_display.py
@@ -22,14 +22,16 @@ def Pad(invoer): # geeft het pad naar de afbeelding van de kaart
 def Afbeelding(invoer): # laadt de afbeelding van de kaart die wordt ingevoerd. Deze afbeelding wordt nog niet afgebeeld, maar slechts ingeladen.
     return pygame.image.load(Pad(invoer)).convert() # wat .convert() dot is lastig uit te leggen, maar dit zorgt ervoor dat het laden van alle pixels sneller gaat.
 
-tekst = ''
-font = pygame.font.Font(None, 32)
-input_box = pygame.Rect(100, 100, 140, 32)
-kleur = pygame.Color('lightskyblue3')
+font = pygame.font.SysFont('Arial', 28)
+kleur = pygame.Color('white')
 kaart_keuze = []
 t0 = time.time()
 begonnen = False
-tekst_invoer = pygame.Rect(630, 400, 40, 32)
+tijd_invoer = ''
+tijd_invoer_rh = pygame.Rect(630, 400, 40, 32)
+tijd_opvraag = font.render('Hoeveel tijd wil je jezelf geven om een set te vinden? Voer een waarde in tussen 1 en 99 seconden.', True, kleur)
+set_invoer = ''
+set_invoer_rh = pygame.Rect(10, 700, 140, 32)
 
 while not begonnen:
     for event in pygame.event.get():
@@ -37,18 +39,22 @@ while not begonnen:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN and len(tekst) > 0:
-                tijd = int(tekst)
-                tekst = ''
+            if event.key == pygame.K_RETURN and len(tijd_invoer) > 0:
+                tijd = int(tijd_invoer)
+                tijd_invoer = ''
                 begonnen = True
-            elif event.key == pygame.K_BACKSPACE and len(tekst) > 0:
-                scherm.fill(pygame.Color('black'), tekst_invoer)
-                tekst = tekst[:-1]
-            elif event.unicode in ['0','1','2','3','4','5','6','7','8','9'] and len(tekst) < 2:
-                tekst += event.unicode
-    tekst_surface = font.render(tekst, True, kleur)
-    scherm.blit(tekst_surface, (tekst_invoer.x + 6, tekst_invoer.y + 6, tekst_invoer.width - 12, tekst_invoer.height - 12))
-    pygame.draw.rect(scherm, kleur, tekst_invoer , 2)
+            elif event.key == pygame.K_BACKSPACE and len(tijd_invoer) > 0:
+                scherm.fill(pygame.Color('black'), tijd_invoer_rh)
+                tijd_invoer = tijd_invoer[:-1]
+            elif event.unicode in ['0','1','2','3','4','5','6','7','8','9'] and len(tijd_invoer) < 2:
+                tijd_invoer += event.unicode
+    
+    scherm.blit(tijd_opvraag, (190,340))
+    
+    tijd_invoer_surface = font.render(tijd_invoer, True, kleur)
+    scherm.blit(tijd_invoer_surface, (tijd_invoer_rh.x + 6, tijd_invoer_rh.y , tijd_invoer_rh.width - 12, tijd_invoer_rh.height - 12))
+    
+    pygame.draw.rect(scherm, kleur, tijd_invoer_rh , 2)
     
     pygame.display.update()
 
@@ -67,39 +73,55 @@ while begonnen: # deze loop wordt gerund terwijl het spel gespeeld wordt, tot he
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
-                if tekst == 'VindSets':
+                if set_invoer == 'VindSets':
                     '''cheatcode weghalen wanneer we klaar zijn??'''
                     print(set.VindSets(Kaarten))
                 else:
-                    kaart_keuze.append(tekst)
-                tekst = ''
-                scherm.fill(pygame.Color("black"), (input_box.x + 450, input_box.y + 5, 140, 32))
-            elif event.key == pygame.K_BACKSPACE and len(tekst) > 0:
-                tekst = tekst[:-1]
-                scherm.fill(pygame.Color("black"), (input_box.x + 450, input_box.y + 5, 140, 32))
-            elif len(tekst) < 8:
-                '''bovenstaande 8 kan een 2 worden als de cheatcode weggehaald wordt'''
-                tekst += event.unicode
+                    kaart_keuze.append(set_invoer)
+                set_invoer = ''
+                scherm.fill(pygame.Color("black"), (set_invoer_rh.x, set_invoer_rh.y, 140, 32))
+            elif event.key == pygame.K_BACKSPACE and len(set_invoer) > 0:
+                set_invoer = set_invoer[:-1]
+                scherm.fill(pygame.Color("black"), (set_invoer_rh.x, set_invoer_rh.y, 140, 32))
+            elif len(set_invoer) < 8:
+                '''bovenstaande 8 kan een 2 worden als de cheatcode weggehaald wordt.
+                Ook kan je hier in zetten dat de event.unicode een cijfer moet zijn'''
+                set_invoer += event.unicode
     # Render the current text.
-    txt_surface = font.render(tekst, True, kleur)
+    set_invoer_surface = font.render(set_invoer, True, kleur)
     
     ''' box groter maken als tekst lang is mogen we van mij weghalen.
     invoer wordt toch niet langer dan 2 cijfers, misschien kunnen we dit er nog in programmeren?
     Ik vind dit iig een beetje onoverzichtelijk en vooral ook overbodig.
     '''
-    # Resize the box if the text is too long.
-    width = max(200, txt_surface.get_width()+10)
-    input_box.w = width
     # Blit the text.
-    scherm.blit(txt_surface, (input_box.x + 450, input_box.y + 5))
+    scherm.blit(set_invoer_surface, (set_invoer_rh.x, set_invoer_rh.y))
     # Blit the input_box rect.
-    pygame.draw.rect(scherm, kleur, (input_box.x + 450, input_box.y + 5, input_box.w, input_box.height) , 2)
-
-    if len(kaart_keuze) == 3:
+    pygame.draw.rect(scherm, kleur, (set_invoer_rh.x, set_invoer_rh.y, set_invoer_rh.width, set_invoer_rh.height) , 2)
+    
+    if len(kaart_keuze) == 0:  # Beeldt de opvraag van de set af
+        set_opvraag_surface = font.render('Voer het kaartnummer in van de eerste kaart van je gevonden set:', True, kleur)
+    elif len(kaart_keuze) == 1:
+        set_opvraag_surface = font.render('Voer het kaartnummer in van de tweede kaart van je gevonden set:', True, kleur)
+    else:
+        set_opvraag_surface = font.render('Voer het kaartnummer in van de derde kaart van je gevonden set:', True, kleur)
+    scherm.fill(pygame.Color("black"), (10, 650, 690, 35))
+    scherm.blit(set_opvraag_surface, (10, 650))
+    
+    if len(kaart_keuze) == 0:
+        kaart_keuze_string = 'Ingevoerde kaarten: '
+    elif len(kaart_keuze) == 1:
+        kaart_keuze_string = 'Ingevoerde kaarten: ' + kaart_keuze[0]
+    elif len(kaart_keuze) == 2:
+        kaart_keuze_string = 'Ingevoerde kaarten: ' + kaart_keuze[0] + ', ' + kaart_keuze[1]
+    kaart_keuze_surface = font.render(kaart_keuze_string, True, kleur)
+    scherm.fill(pygame.Color("black"), (10, 740, 280, 35))
+    scherm.blit(kaart_keuze_surface, (10, 740))
+    
+    if len(kaart_keuze) == 3:  # Checkt of de ingevoerde kaarten een set zijn
         Kaart1 = set.Kaart(Kaarten[int(kaart_keuze[0]) -1])
         Kaart2 = set.Kaart(Kaarten[int(kaart_keuze[1]) -1])
         Kaart3 = set.Kaart(Kaarten[int(kaart_keuze[2]) -1])
-        
         if set.IsSet(Kaart1, Kaart2, Kaart3):
             for i in range(3):
                 if len(Pot) != 0:
@@ -120,10 +142,7 @@ while begonnen: # deze loop wordt gerund terwijl het spel gespeeld wordt, tot he
             GevondenSet = []
             set.vervang_kaarten(Kaarten, Pot)
         t0 = time.time()
-        
-          
-             
-        
+    
     '''Kaarten rooster ziet er als volgt uit:
     1 4 7 10
     2 5 8 11
